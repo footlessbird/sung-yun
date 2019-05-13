08f709fb3304e7233d821f348bee063017644b47
footlessbird <>
2019-05-13 00:48:01 -0700

    Roll back

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,28 @@
 const path = require("path")
-const { createBlogs, createWorks } = require("./src/gatsby/pageCreator")
-const gatsbyNodeGraphQL = require("./src/gatsby/gatsbyNodeGraphQL")
-
-const wrapper = promise =>
-  promise.then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-    console.log('for line 11')
-    console.log(result)
-    return result
-  })
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve("./src/templates/blog.js")
-  const workTemplate = path.resolve("./src/templates/work.js")
-
-  const result = await wrapper(
-    graphql(`
-    {
-      ${gatsbyNodeGraphQL}
+  const res = await graphql(`
+    query {
+      allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
     }
-    `)
-  )
-  console.log('for line 28')
-  console.log(result)
-  createBlogs(result.data.blogs.edges, createPage, blogTemplate)
-  createWorks(result.data.works.edges, createPage, workTemplate)
+  `)
+
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
 }
+
