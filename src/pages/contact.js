c3b7354b4d026f10d6c6153208c94bdfdc1f5711
footlessbird <>
2019-05-14 22:55:09 -0700

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Head from '../components/head'
+import Head from "../components/head"
 
 const Contact = () => {
   return (
     <Layout>
-      <Head title="Contact"/>
-      <h1></h1>
-     
+      <Head title="Contact" />
+      <h2>Contact</h2>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input name="name" placeholder="Your Name" type="text" />
+        <button>Send</button>
+      </form>
     </Layout>
   )
 }
