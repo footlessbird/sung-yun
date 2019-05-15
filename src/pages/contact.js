7df0993bb9fa3f181eee78488e79ef076c77bf70
footlessbird <>
2019-05-14 23:11:42 -0700

    form test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,8 +10,33 @@ const Contact = () => {
       <Head title="Contact" />
       <h2>Contact</h2>
       <form name="contact" method="POST" data-netlify="true">
-        <input name="name" placeholder="Your Name" type="text" />
-        <button>Send</button>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
     </Layout>
   )
