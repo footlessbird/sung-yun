9b8cb0385a2682542d0eb63d7c64ce19a1a4875e
footlessbird <>
2019-05-14 23:56:46 -0700

    form testing...

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,27 +9,20 @@ const Contact = () => {
     <Layout>
       <Head title="Contact" />
       <h2>Contact</h2>
-      <form
-        name="contact"
-        method="POST"
-        data-netlify-recaptcha="true"
-        data-netlify="true"
-      >
-        <p>
-          <label>
-            Email: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
-        </p>
-        <div data-netlify-recaptcha="true" />
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+  <p class="hidden">
+    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+  </p>
+  <p>
+    <label>Email: <input type="text" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     </Layout>
   )
 }
